ce9394d94346f624492a3fe7cf75fc4812473fc7
RaduRS <rsrusu90@gmail.com>
2022-02-03 11:03:52 +0000

    updating the app with shop page

--- src/App.js
+++ src/App.js
@@ -2,12 +2,14 @@ import React from "react";
 import "./App.css";
 import Homepage from "./pages/homepage/homepage.component";
 import { Routes, Route } from "react-router-dom";
+import ShopPage from "./pages/shop/shop.component";
 
 function App() {
   return (
     <div>
       <Routes>
         <Route path="/" element={<Homepage />} />
+        <Route path="/shop" element={<ShopPage />} />
       </Routes>
     </div>
   );
